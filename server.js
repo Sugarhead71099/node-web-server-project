3fb02853454badde40099f9a6ce94a29fa72c8a2
Braijon <developerbraijon@gmail.com>
2018-04-23 23:02:24 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,63 @@
+/*** Configuration ***/
+
+const express = require('express'),
+	  hbs = require('hbs'),
+	  fs = require('fs');
+
+// Start express server
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+hbs.registerHelper( 'getCurrentYear', () => new Date().getFullYear() );
+hbs.registerHelper( 'screamIt', (text) => text.toUpperCase() );
+
+
+
+/*** Middleware ***/
+app.use((request, response, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${request.method} ${request.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', `${log}\n`, (error) => {
+		if ( error )
+		{
+			console.log('Unable to append to server.log.');
+		}
+	});
+
+	next();
+});
+
+// app.use( (request, response, next) => response.render('maintenance') );
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+/*** Routing ***/
+app.get('/', (request, response) => {
+	response.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome To The Homepage',
+	});
+});
+
+app.get('/about', (request, response) => {
+	response.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
+});
+
+app.get('/bad', (request, response) => {
+	response.send({
+		errorMessage: 'Unable to handle request',
+	});
+});
+
+
+
+// Listen on port
+app.listen(3000);
